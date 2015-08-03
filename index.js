ec5cc4736b7c5a659255f069c7287dfefdb54d6b
andrepolischuk <andre.polischuk@gmail.com>
2015-08-03 16:28:10 +0300

    Remove extra condition

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 import EPSILON from 'number-epsilon';
-
-export default (a, b, error = 0) => {
-  const diff = Math.abs(a - b);
-  return diff >= 0 && diff - error < EPSILON;
-};
+export default (a, b, error = 0) => Math.abs(a - b) - error < EPSILON;
