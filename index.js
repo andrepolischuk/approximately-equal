bb7b3861057c2501ceca136e6d19293cd67e6ccd
andrepolischuk <andre.polischuk@gmail.com>
2015-12-07 16:15:02 +0300

    Return Number.EPSILON polyfill

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-export default (a, b, error = 0) => Math.abs(a - b) - error < Number.EPSILON;
+import EPSILON from 'number-epsilon';
+export default (a, b, error = 0) => Math.abs(a - b) - error < EPSILON;
