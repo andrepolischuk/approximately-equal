915dd768c781737ee060f8b3bf202cf22668b247
Andrey Polischuk <andre.polischuk@gmail.com>
2016-08-15 00:50:19 +0300

    Add function name for error tracing

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 import EPSILON from 'number-epsilon';
-export default (a, b, error = 0) => Math.abs(a - b) - error < EPSILON;
+
+export default function approximatelyEqual(a, b, error = 0) {
+  return Math.abs(a - b) - error < EPSILON;
+}
